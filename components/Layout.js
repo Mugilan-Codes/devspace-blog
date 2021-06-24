167593d82552bfb0b3999e996ae36b67e26674fd
Mugilan <mugilancodes@gmail.com>
2021-06-24 20:14:13 +0530

    layout component

---
+++ components/Layout.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+
+        <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
+      </Head>
+
+      <main className='container mx-auto my-7'>{children}</main>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: 'Welcome to DevSpace',
+  keywords: 'development, coding, programming, web',
+  description:
+    'The one stop for all the best news and information in development',
+};
