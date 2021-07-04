71884a73243177caf08db65d5a8ac25b478852c4
Mugilan <mugilancodes@gmail.com>
2021-07-04 10:47:39 +0530

    about & 404 page

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,21 @@
 import Layout from '@/components/Layout';
 
 export default function AboutPage() {
-  return <Layout title='About Page'></Layout>;
+  return (
+    <Layout title='About Page'>
+      <h1 className='text-5xl border-b-4 pb-5 font-bold'>About</h1>
+
+      <div className='bg-white shadow-md rounded-lg px-10 py-6 mt-6'>
+        <h3 className='text-2xl mb-5'>DevSpace Blog</h3>
+
+        <p className='mb-3'>
+          This is a blog with Next.js and markdown following Brad Traversy
+        </p>
+
+        <p>
+          <span className='font-bold'>Version 1.0.0</span>
+        </p>
+      </div>
+    </Layout>
+  );
 }
---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import Layout from '@/components/Layout';
+
+export default function BlogPage() {
+  return (
+    <Layout>
+      <h1 className='text-9xl'>Blog</h1>
+    </Layout>
+  );
+}
