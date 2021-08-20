d069ebd3401d225e77bd08a1de09cdf4319bf895
Mugilan <mugilancodes@gmail.com>
2021-08-20 16:43:11 +0530

    production bug fix :bug:

--- pages/api/search.js
+++ pages/api/search.js
@@ -3,11 +3,7 @@ import Fuse from 'fuse.js';
 import { getSortedPosts } from '@/lib/posts';
 import { IS_PROD } from '@/config/index';
 
-// const posts = IS_PROD ? require('../../cache/data').posts : getSortedPosts();
-const posts =
-  process.env.NODE_ENV === 'production'
-    ? require('../../cache/data').posts
-    : getSortedPosts();
+const posts = IS_PROD ? require('../../cache/data').posts : getSortedPosts();
 
 const fuse = new Fuse(posts, {
   includeMatches: true,
@@ -23,16 +19,24 @@ const fuse = new Fuse(posts, {
 });
 
 export default (req, res) => {
-  const searchTerm = `'${req.query.q}`; // Search for items that include(') the query term
+  // const searchTerm = `'${req.query.q}`; // Search for items that include(') the query term
 
-  const fuseResults = fuse.search(searchTerm);
-  // console.log({ fuseResults });
+  // const fuseResults = fuse.search(searchTerm);
+  // // console.log({ fuseResults });
 
-  // HACK: rewrited to easily access the necessary content
-  const results = fuseResults.map((fres) => ({
-    slug: fres.item.slug,
-    frontmatter: fres.item.frontmatter,
-  }));
+  // // HACK: rewrited to easily access the necessary content
+  // const results = fuseResults.map((fres) => ({
+  //   slug: fres.item.slug,
+  //   frontmatter: fres.item.frontmatter,
+  // }));
+
+  const results = posts.filter(
+    ({ frontmatter: { title, excerpt, category, author } }) =>
+      title.toLowerCase().indexOf(req.query.q) != -1 ||
+      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+      category.toLowerCase().indexOf(req.query.q) != -1 ||
+      author.toLowerCase().indexOf(req.query.q) != -1
+  );
 
   res.status(200).json(JSON.stringify({ results }));
 };
